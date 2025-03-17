6515803e4fb39f26b9f8e2cf4d02994ca6b31576
Чеботарев Дмитрий Степанович <chebotarev.d.s@mac205-01.ouiit.local>
2025-03-17 13:36:50 +0300

    Final_update

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from aiogram import Bot, Dispatcher, types, Router, filters, F
 import asyncio
+from config import bot_api
 # Токен вашего бота
-API_TOKEN = "7946007308:AAEbWC6c6uC7q5RjFse7FLpLYkdGYPsaSeY"
+API_TOKEN = bot_api
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
@@ -13,10 +14,16 @@ start_router = Router()
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Я echo bot. Напиши мне что-нибудь, и я повторю.")
 
+# Обработчик команды /help
+@start_router.message(filters.Command("help"))
+async def send_help(message: types.Message):
+    help_text = "Доступные команды:\n/start - Начать диалог с ботом\n/help - Получить список команд"
+    await message.reply(help_text)
+
 # Обработчик текстовых сообщений
 @start_router.message(F.text)
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    await message.answer(f"Вы сказали: {message.text}")
 
 async def main():
     dp.include_router(start_router)
